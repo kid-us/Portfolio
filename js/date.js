c640d014f57cac8bf807c23373d90179cb08ecd6
Kidus <98009847+kid-us@users.noreply.github.com>
2023-10-20 12:18:14 -0700

    Update date.js

--- js/date.js
+++ js/date.js
@@ -1,1 +1,54 @@
+window.document.onload = startTime();
 
+const weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const month = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "November",
+  "Dec",
+];
+
+const d = new Date();
+
+let day = weekday[d.getDay()];
+let year = d.getFullYear();
+let m = month[d.getMonth()];
+
+let hour = d.getHours();
+let minute = d.getMinutes();
+
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById("hour").innerText = h;
+  document.getElementById("minute").innerText = m;
+  document.getElementById("second").innerText = s;
+  // Mobile Time
+  document.getElementById("mobile-hour").innerText = h;
+  document.getElementById("mobile-minute").innerText = m;
+  setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+document.getElementById("year").innerText = "< " + year + " />";
+document.getElementById("weekend").innerText = day;
+document.getElementById("date").innerText = d.getDate();
+document.getElementById("month").innerText = m;
